8afa4958993618447c93341718252f456807d2a3
helloWorld141 <nghia002@outlook.com>
2018-02-27 16:17:38 +0800

    .

--- spark_cluster.py
+++ spark_cluster.py
@@ -2,6 +2,12 @@ import paramiko, json
 
 port = 22
 
+def exe(cmd, args, ssh):
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    print("Remote stdout:\n" + stdout.read().decode('utf-8'))
+    print("Remote stderr:\n" + stderr.read().decode('utf-8'))
+    return stdin, stdout, stderr
+
 if __name__ == "__main__":
     file = open('spark_cluster.conf').read()
     config = json.loads(file)
@@ -19,13 +25,7 @@ if __name__ == "__main__":
     s.connect(slaves[0], port, pkey=key, auth_timeout=60)
     print('ssh connected')
     ######
-    stdin, stdout, stderr = s.exec_command('ifconfig')
-    print(stderr.read())
-    print("Remote stdout:\n" + stdout.read().decode('utf-8'))
-    print("Remote stderr:\n" + stderr.read().decode('utf-8'))
+    stdin, stdout, stderr = exe('ifconfig')
     if stderr.read() != b'':
-        stdin, stdout, stderr = s.exec_command('sudo apt-get install net-tools')
-    print('command executed')
-    print("Remote stdout:\n" + stdout.read().decode('utf-8'))
-    print("Remote stderr:\n" + stderr.read().decode('utf-8'))
+        stdin, stdout, stderr = exe('sudo apt-get install net-tools')
     s.close()
